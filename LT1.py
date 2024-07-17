1307fdd26e947fa9b0ba68df10a7f1c8ebcd9d7e
StVaSt <strelyabin@gmail.com>
2024-07-17 22:19:19 +0300

    Test

--- LT1.py
+++ LT1.py
@@ -1,2 +1,6 @@
-print("Hellow")
-print("Hellow 2")
+first_name = input("Enter your name: ")
+last_name = input("Enter your surname: ")
+age = int(input("Enter your age: "))
+
+print(f"{first_name} {last_name}\n"*age)
+
